a17d67e3a7f521309558eaba94690e5c69dd4e2d
KeithFlores <kiFlores@mcm.edu.ph>
2025-11-28 23:07:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
-const toggleButton = document.getElementById('theme-toggle');
+const themeToggleBtn = document.getElementById('theme-toggle');
+const videoToggleBtn = document.getElementById('video-toggle');
+const bgVideo = document.getElementById('bg-video');
 const body = document.body;
-toggleButton.addEventListener('click', () => {
+const header = document.querySelector('header');
+const allContent = document.querySelector('.all'); 
+
+const exitVideoBtn = document.getElementById('exit-video-btn'); 
+
+
+themeToggleBtn.addEventListener('click', () => {
     body.classList.toggle('dark');
 });
+
+
+const toggleVideo = () => {
+    if (!body.classList.contains('video-active')) {
+
+        body.classList.add('video-active');
+        bgVideo.play();
+        videoToggleBtn.textContent = "Hide Video Background";
+        
+
+        header.style.opacity = '0';
+        allContent.style.opacity = '0';
+
+    } else {
+        bgVideo.pause();
+        bgVideo.currentTime = 0;
+        body.classList.remove('video-active');
+        videoToggleBtn.textContent = "Show Video Background";
+
+
+        header.style.opacity = '1';
+        allContent.style.opacity = '1';
+    }
+};
+
+
+videoToggleBtn.addEventListener('click', toggleVideo);
+exitVideoBtn.addEventListener('click', toggleVideo);
